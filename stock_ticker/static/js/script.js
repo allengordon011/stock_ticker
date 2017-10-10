afa92e3e300009e24e40f5a44a6caea9a3311fb6
trig010 <trig.code@gmail.com>
2017-10-09 23:46:24 -0500

    stock update working

--- stock_ticker/static/js/script.js
+++ stock_ticker/static/js/script.js
@@ -2,9 +2,7 @@
 $(function() {
     $('#form-search').on('submit', function(e) {
         e.preventDefault();
-        var stock = $('#stock-symbol').val();
-        // console.log('STOCK: ', stock)
-
+        let stock = $('#stock-symbol').val();
         $.ajax({
             url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + stock + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=',
             type: 'POST',
@@ -21,11 +19,14 @@ $(function() {
                     contentType: "application/json; charset=utf-8",
                     success: function() {
                         console.log('Successful API post');
+                        location.reload();
+                        
                     },
                     error: function(error) {
                         console.log(error);
                     }
                 });
+                
                 }
             },
             error: function(error) {
@@ -36,43 +37,38 @@ $(function() {
     });
 });
 
-//stock update
+//stocks update
 function update() {
-    // fetch('/updateStocks')
-    // .then((res)=>{ console.log(res) })
     var stocksToUpdate = document.getElementById('stocksToUpdate').innerHTML
     if(stocksToUpdate){
         let mod= stocksToUpdate.replace(/'/g, '"'); //remove outer quotation marks
         let obj = JSON.parse(mod) //convert string
-        obj.forEach(function(stock){console.log(stock.symbol)})} else {console.log('NO STOCK TO UPDATE')}
-    // $.ajax({
-    //     url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + stock + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=',
-    //     type: 'POST',
-    //     success: function(response) {
-    //         let quote = response.query.results.quote;
-    //         if(quote.LastTradePriceOnly == null) {
-    //             console.log('Bad Search');
-    //             $('.hidden-flash').show(500);
-    //         } else {
-    //         $.ajax({
-    //             url: '/update',
-    //             data: JSON.stringify(quote),
-    //             type: 'POST',
-    //             contentType: "application/json; charset=utf-8",
-    //             success: function() {
-    //                 console.log('Successful Stock Prices Update');
-    //             },
-    //             error: function(error) {
-    //                 console.log(error);
-    //             }
-    //         });
-    //         }
-    //     },
-    //     error: function(error) {
-    //         console.log(error);
-    //     }
-    // });
-
+        obj.forEach(function(stock){
+            $.ajax({
+                url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + stock.symbol + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=',
+                type: 'POST',
+                success: function(response) {
+                    let quote = response.query.results.quote;
+                    $.ajax({
+                        url: '/update',
+                        data: JSON.stringify(quote),
+                        type: 'POST',
+                        contentType: "application/json; charset=utf-8",
+                        success: function() {
+                            console.log('Successful Stock Prices Update');
+                            location.reload();
+                        },
+                        error: function(error) {
+                            console.log(error);
+                        }
+                    });
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        })
+    } else {console.log('NO STOCK TO UPDATE')}
 };
 
 //stock delete
@@ -88,6 +84,7 @@ $(function() {
             contentType: "application/json; charset=utf-8",
             success: function() {
                 console.log('Successful DELETE post');
+                location.reload();
             },
             error: function(error) {
                 console.log(error);
@@ -101,18 +98,8 @@ $(document).ready(function(){
     setTimeout(function() {
     $(".flash").delay(3000).fadeOut();
     });
+    
     setTimeout(function() {
         update();
-        }, 1000);
-    setTimeout(function() {
-        console.log('REFRESH')
-        location.reload();
       }, 30000);
-});
-
-// reload after stock search
-$(document).ajaxStop(function(){
-    setTimeout(function(){
-      window.location.reload();
-    }, 2000);
 });
